edb697343aa79709052a8852e71c9d74ab562dc9
Brett L. Schuchert <brett.schuchert@ext.biomerieous.com>
2022-02-09 11:55:03 -0600

    Plugin now has property extensions

---
+++ plugin/src/main/groovy/org/shoe/Gradle_plugin1PluginExtension.groovy
@@ -0,0 +1,8 @@
+package org.shoe
+
+import org.gradle.api.provider.Property
+
+abstract class Gradle_plugin1PluginExtension {
+    abstract Property<String> getMessage()
+    abstract Property<String> getGreeter()
+}
--- plugin/src/main/groovy/org/shoe/Gradle_plugin_1Plugin.groovy
+++ plugin/src/main/groovy/org/shoe/Gradle_plugin_1Plugin.groovy
@@ -5,10 +5,10 @@ import org.gradle.api.Plugin
 
 class Gradle_plugin_1Plugin implements Plugin<Project> {
     void apply(Project project) {
-        // Register a task
+        def extension = project.extensions.create('greeting', Gradle_plugin1PluginExtension)
         project.tasks.register("greeting") {
             doLast {
-                println("Hello from plugin 'org.shoe.greeting'")
+                println "${extension.message.get()} from ${extension.greeter.get()}"
             }
         }
     }
