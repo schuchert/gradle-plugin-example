4204a1f052e2c59b4b420437ea43e10bdbfb104b
Brett L. Schuchert <brett.schuchert@ext.biomerieous.com>
2022-02-09 17:11:33 -0600

    Got properties and default values

--- plugin/src/main/groovy/org/shoe/GitBuildVersioningPluginExtension.groovy
+++ plugin/src/main/groovy/org/shoe/GitBuildVersioningPluginExtension.groovy
@@ -1,8 +1,29 @@
 package org.shoe
 
+
+import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
-abstract class GitBuildVersioningPluginExtension {
-    abstract Property<String> getMessage()
-    abstract Property<String> getGreeter()
+import javax.naming.spi.ObjectFactory
+
+class GitBuildVersioningPluginExtension {
+    Property<String> message
+    Property<String> greeter
+
+    GitBuildVersioningPluginExtension(Project project) {
+        message = project.objects.property(String)
+        if (project.hasProperty('message')) {
+            greeter = message.convention(project.provider({ project.greeter }))
+        } else {
+            message.convention('Hello')
+        }
+
+        greeter = project.objects.property(String)
+        if (project.hasProperty('greeter')) {
+            greeter = message.convention(project.provider({ project.greeter }))
+        } else {
+            message.convention('The Great Unknown')
+        }
+    }
+
 }
