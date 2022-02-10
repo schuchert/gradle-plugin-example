534c8e31eb06ed0a918e0532e29ecad6d336a84d
Brett L. Schuchert <brett.schuchert@ext.biomerieous.com>
2022-02-10 09:17:26 -0600

    Extracted method to init property

--- plugin/src/main/groovy/org/shoe/GitBuildVersioningPluginExtension.groovy
+++ plugin/src/main/groovy/org/shoe/GitBuildVersioningPluginExtension.groovy
@@ -4,26 +4,25 @@ package org.shoe
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
-import javax.naming.spi.ObjectFactory
-
 class GitBuildVersioningPluginExtension {
     Property<String> message
     Property<String> greeter
 
     GitBuildVersioningPluginExtension(Project project) {
-        message = project.objects.property(String)
-        if (project.hasProperty('message')) {
-            greeter = message.convention(project.provider({ project.greeter }))
+        message = initProperty(project, 'message', { project.message }, 'Hello')
+        greeter = initProperty(project, 'greeter', { project.greeter }, 'The Great Unknown')
+    }
+
+    private Property<String> initProperty(Project project, String propertyName, groovy.lang.Closure fromProject, String defaultValue) {
+        def property = project.objects.property(String)
+
+        if (project.hasProperty(propertyName)) {
+            property.convention(project.provider(fromProject))
         } else {
-            message.convention('Hello')
+            property.convention(defaultValue)
         }
 
-        greeter = project.objects.property(String)
-        if (project.hasProperty('greeter')) {
-            greeter = message.convention(project.provider({ project.greeter }))
-        } else {
-            message.convention('The Great Unknown')
-        }
+        return property
     }
 
 }
